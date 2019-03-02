2175883b8ba658990fb632d73eb21e496a8267cf
Kent Madsen <20760795+KentMadsen@users.noreply.github.com>
2019-03-02 23:54:22 +0100

    a

---
+++ templatea.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+      <head>
+            <?php
+                  include_once "./skeleton/meta.html";
+                  include_once "./skeleton/libraries.html";
+                  include_once "./skeleton/fonts.html";
+                  include_once "./skeleton/css.html";
+            ?>
+            <title>
+                  Home - SEO
+            </title>
+      </head>
+
+      <body>
+            <?php
+                  include_once "./skeleton/header.html";
+            ?>
+
+            <main>
+              <section class="document-section">
+
+              </section>
+              <section class="document-section">
+
+              </section>
+              <section class="document-section">
+
+              </section>
+            </main>
+
+            <?php
+                  include_once "./skeleton/footer.html";
+            ?>
+      </body>
+
+      <div class="ignore" hidden>
+        <?php
+              include_once "./skeleton/scripts.html";
+        ?>
+      </div>
+</html>
