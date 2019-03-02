19858279b57cdebc28e9c15166b659983e7488cf
Kent Madsen <20760795+KentMadsen@users.noreply.github.com>
2019-03-02 17:40:15 +0100

    a

---
+++ articles.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+      <head>
+            <?php
+                  include_once "./skeleton/meta.html";
+                  include_once "./skeleton/libraries.html";
+                  include_once "./skeleton/fonts.html";
+                  include_once "./skeleton/css.html";
+            ?>
+            <title>
+                  Home - Articles
+            </title>
+      </head>
+
+      <body>
+            <?php
+                  include_once "./skeleton/header.html";
+            ?>
+
+            <main>
+
+            </main>
+
+            <?php
+                  include_once "./skeleton/footer.html";
+            ?>
+      </body>
+
+      <div class="ignore" hidden>
+        <?php
+              include_once "./skeleton/scripts.html";
+        ?>
+      </div>
+</html>
---
+++ css/readme.md
@@ -0,0 +1,2 @@
+# CSS Folder
+Place where all the css files will be outputted
