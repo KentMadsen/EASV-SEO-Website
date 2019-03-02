5f241774e2367da3b1232898d281564ba1fea636
Kent Madsen <20760795+KentMadsen@users.noreply.github.com>
2019-03-02 13:45:07 +0100

    gitignore added

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+      <head>
+            <meta charset="UTF-8">
+            
+            <meta name="viewport" 
+                  content="width=device-width, initial-scale=1.0">
+
+            <meta http-equiv="X-UA-Compatible" 
+                  content="ie=edge">
+
+            <?php 
+                  include_once "./skeleton/fonts.html";
+                  include_once "./skeleton/css.html";
+            ?>
+
+            <title>
+                  SEO Main
+            </title>
+      
+      </head>
+      <body>
+            <?php 
+                  include_once "./skeleton/header.html";
+            ?>
+
+            <main> 
+
+            </main>
+
+            <?php 
+                  include_once "./skeleton/footer.html";
+            ?>
+      </body>
+
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-135454516-1"></script>
+      <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-135454516-1');
+      </script>
+</html>
