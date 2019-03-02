27916c752ea1addd44ea09471494d76b5ec3c12f
Kent Madsen <20760795+KentMadsen@users.noreply.github.com>
2019-03-02 15:49:58 +0100

    a

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 <html lang="en">
       <head>
             <?php
-                  include_once "./skeleton/meta.html"; 
+                  include_once "./skeleton/meta.html";
+                  include_once "./skeleton/libraries.html"; 
                   include_once "./skeleton/fonts.html";
                   include_once "./skeleton/css.html";
             ?>
@@ -15,23 +16,15 @@
             <?php 
                   include_once "./skeleton/header.html";
             ?>
-
             <main> 
 
             </main>
-            
             <?php 
                   include_once "./skeleton/footer.html";
             ?>
       </body>
 
-      <!-- Global site tag (gtag.js) - Google Analytics -->
-      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-135454516-1"></script>
-      <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'UA-135454516-1');
-      </script>
+      <?php 
+            include_once "./skeleton/scripts.html";
+      ?>
 </html>
