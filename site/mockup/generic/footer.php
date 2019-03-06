9446272495ef47fdae73ac231eec65edd20d0434
Kent Madsen <20760795+KentMadsen@users.noreply.github.com>
2019-03-06 20:36:16 +0100

    a

--- site/mockup/generic/footer.php
+++ site/mockup/generic/footer.php
@@ -1,195 +1,193 @@
 <footer>
-                  <div class="footer-cover">
-                        <div class="page-boundary">
-                              <!-- Container-->
-                              <nav class="horizontal-link-menu menu-area-container site-menu">
-                                          <!-- Container-->
-                                          <div class="menu-container">
-                                                <ul class="vertical-menu">
-                                                      <li>
-                                                            <p>
-                                                                  Menu
-                                                            </p>
-                                                      </li>
-                                                      <li>
-                                                            <a href="./index.php" 
-                                                               hreflang="en" 
-                                                               class="link overlay-link">
-                                                                  Home
-                                                            </a>
-                                                      </li>
-                                                      <li>
-                                                            <a href="./whatisseo.php" 
-                                                               hreflang="en" 
-                                                               class="link overlay-link">
-                                                                  What is SEO ?
-                                                            </a>
-                                                      </li>
-                                                      <li>
-                                                            <a href="./seoinpractice.php" 
-                                                               hreflang="en" 
-                                                               class="link overlay-link">
-                                                                  SEO in practice
-                                                            </a>
-                                                      </li>
-                                                      <li>
-                                                            <a href="./theme.php" 
-                                                               hreflang="en" 
-                                                               class="link overlay-link">
-                                                                  Themes
-                                                            </a>
-                                                      </li>
-                                                </ul>
-                                          </div>
+<div class="footer-cover">
+<div class="page-boundary">
+<!-- Container-->
+<nav class="horizontal-link-menu menu-area-container site-menu">
+<!-- Container-->
+<div class="menu-container">
+<ul class="vertical-menu">
+<li>
+<p>
+Menu
+</p>
+</li>
+<li>
+<a href="./index.php" 
+hreflang="en" 
+class="link overlay-link">
+Home
+</a>
+</li>
+<li>
+<a href="./whatisseo.php" 
+hreflang="en" 
+class="link overlay-link">
+What is SEO ?
+</a>
+</li>
+<li>
+<a href="./seoinpractice.php" 
+hreflang="en" 
+class="link overlay-link">
+SEO in practice
+</a>
+</li>
+<li>
+<a href="./theme.php" 
+hreflang="en" 
+class="link overlay-link">
+Themes
+</a>
+</li>
+</ul>
+</div>
                                           
-                                          <!-- Container-->
-                                          <div class="menu-container">
-                                                <ul class="vertical-menu">
-                                                      <li>
-                                                            <p>
-                                                                  Help
-                                                            </p>
-                                                      </li>
+<!-- Container-->
+<div class="menu-container">
+<ul class="vertical-menu">
+<li>
+<p>
+Help
+</p>
+</li>
                                                       
-                                                      <li>
-                                                            <a href="#" 
-                                                               hreflang="en" 
-                                                               class="link overlay-link">
-                                                                  Getting Started
-                                                            </a>
-                                                      </li>
+<li>
+<a href="#" 
+hreflang="en" 
+class="link overlay-link">
+Getting Started
+</a>
+</li>
                                                       
-                                                      <li>
-                                                            <a href="#" 
-                                                               hreflang="en" 
-                                                               class="link overlay-link">
-                                                                  References/Citations  
-                                                            </a>
-                                                      </li>
+<li>
+<a href="#" 
+hreflang="en" 
+class="link overlay-link">
+References/Citations  
+</a>
+</li>
                                                       
-                                                      <li>
-                                                            <a href="#" 
-                                                               hreflang="en" 
-                                                               class="link overlay-link">
-                                                                  Code
-                                                            </a>
-                                                      </li>
+<li>
+<a href="#" 
+hreflang="en" 
+class="link overlay-link">
+Code
+</a>
+</li>
 
-                                                      <li>
-                                                            <a href="#" 
-                                                               hreflang="en" 
-                                                               class="link overlay-link">
-                                                                  Dictionary
-                                                            </a>
-                                                      </li>
-                                                </ul>
-                                          </div>
+ <li>
+ <a href="#" 
+ hreflang="en" 
+ class="link overlay-link">
+ Dictionary
+ </a>
+ </li>
+ </ul>
+</div>
 
-                                          <!-- Container-->
-                                          <div class="menu-container">
-                                                <ul class="vertical-menu">
-                                                      <!-- Not sure about domain !-->
-                                                      <li>
-                                                            <p>
-                                                               Misc   
-                                                            </p>
-                                                      </li>
+ <!-- Container-->
+ <div class="menu-container">
+ <ul class="vertical-menu">
+ <!-- Not sure about domain !-->
+ <li>
+ <p>
+ Misc   
+ </p>
+ </li>
 
-                                                      <li>
-                                                            <a href="#"
-                                                               hreflang="en" 
-                                                               class="link overlay-link">
-                                                                  Sitemap
-                                                            </a>
-                                                      </li>
-                                                      <li>
-                                                            <a href="#"
-                                                               hreflang="en" 
-                                                               class="link overlay-link">
-                                                                  Tools & Services
-                                                            </a>
-                                                      </li>
-                                                      <li>
-                                                            <a href="./links.php"
-                                                               hreflang="en" 
-                                                               class="link overlay-link">
-                                                                  SEO's link relations  
-                                                            </a>
-                                                      </li>
-                                                      <li>
-                                                            <a 
-                                                               hreflang="en" 
-                                                               class="link overlay-link">
-                                                                  
-                                                            </a>
-                                                      </li>
-                                                </ul>
-                                          </div>
+<li>
+<a href="#"
+hreflang="en" 
+class="link overlay-link">
+Sitemap
+</a>
+</li>
 
-                                          <!-- Container-->
-                                          <div class="menu-container">
-                                                <div class="company-information">
-                                                      <p> 
-                                                            <a href="./index.php" 
-                                                               hreflang="en" 
-                                                               class="link overlay-link">
-                                                                  SEO
-                                                            </a> 
-                                                      </p>
+<li>
+<a href="#"
+hreflang="en" 
+class="link overlay-link">
+Tools & Services
+</a>
+</li>
 
-                                                      <p>
-                                                            Created by
-                                                            <a href="https://www.designermadsen.com/" 
-                                                               class="link overlay-link"> 
-                                                                  Kent vejrup Madsen 
-                                                            </a>
-                                                      </p>
-                                                </div>                                              
-                                          </div>
-                              </nav>
-                        </div>
-                  </div>
-                  <!-- -->
-                  <nav>  
-                        <div class="page-boundary footer-misc-navigation">
-                              <ul class="horizontal-menu menu-even site-menu footer-misc ">
-                                    <li>
-                                          <a href="#" 
-                                             hreflang="en" 
-                                             class="link overlay-link-light">
-                                                Contact us
-                                          </a>
-                                    </li>
+<li>
+<a href="./links.php"
+hreflang="en" 
+class="link overlay-link">
+SEO's link relations  
+</a>
+</li>
 
-                                    <li>
-                                          <a href="#" 
-                                             hreflang="en" 
-                                             class="link overlay-link-light">
-                                                Terms of service
-                                          </a>
-                                    </li>
+</ul>
+</div>
 
-                                    <li>
-                                          <a href="#" 
-                                             hreflang="en" 
-                                             class="link overlay-link-light">
-                                                Cookie Policy
-                                          </a>
-                                    </li>
+<!-- Container-->
+<div class="menu-container">
+<div class="company-information">
+<p> 
+<a href="./index.php" 
+hreflang="en" 
+class="link overlay-link">
+SEO
+</a> 
+</p>
 
-                                    <li>
-                                          <a href="#" 
-                                             hreflang="en" 
-                                             class="link overlay-link-light">
-                                                GDPR
-                                          </a>
-                                    </li>
-                              </ul>
-                        </div>
-                  </nav>
-                  <div class="page-boundary footer-misc">
-                        <p>
-                              Copyright 2019 SEO, Kent Madsen All rights reserved
-                        </p>
-                  </div>
-            </footer>
+<p>
+Created by
+<a href="https://www.designermadsen.com/" 
+class="link overlay-link"> 
+Kent vejrup Madsen 
+</a>
+</p>
+</div>                                              
+</div>
+</nav>
+</div>
+</div>
+
+<!-- -->
+<nav>  
+<div class="page-boundary footer-misc-navigation">
+<ul class="horizontal-menu menu-even site-menu footer-misc ">
+<li>
+<a href="#" 
+hreflang="en" 
+class="link overlay-link-light">
+Contact us
+</a>
+</li>
+
+<li>
+<a href="#" 
+hreflang="en" 
+class="link overlay-link-light">
+Terms of service
+</a>
+</li>
+
+<li>
+<a href="#" 
+hreflang="en" 
+class="link overlay-link-light">
+Cookie Policy
+</a>
+</li>
+
+<li>
+<a href="#" 
+hreflang="en" 
+class="link overlay-link-light">
+GDPR
+</a>
+</li>
+</ul>
+</div>
+</nav>
+
+<div class="page-boundary footer-misc">
+<p>
+Copyright 2019 SEO, Kent Madsen All rights reserved
+</p>
+</div>
+</footer>
