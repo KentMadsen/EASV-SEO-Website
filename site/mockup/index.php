4acf3482781c1e0f8c0c134e5dbebb73ee741513
Kent Madsen <20760795+KentMadsen@users.noreply.github.com>
2019-03-06 06:08:57 +0100

    a

--- site/mockup/index.php
+++ site/mockup/index.php
@@ -13,281 +13,81 @@
                   SEO - Home
             </title>
 
-            <link href="https://fonts.googleapis.com/css?family=Actor|Armata|Assistant|Fira+Sans|Karma|Montserrat|Open+Sans|Oswald|Oxygen|Questrial|Quicksand|Raleway|Roboto|Source+Code+Pro" rel="stylesheet">  
+            <?php 
+                  include "./generic/libraries.php";
+            ?>
 
             <link rel="stylesheet" 
                   href="./css/style.css">
       </head>
       <body>
-            <header>    
-                        <nav class="site-nav">            
-                              <div class="page-boundary">
-                                    <ul class="horizontal-menu menu-left site-menu">
-                                          <li>
-                                                <a href="index.php" 
-                                                   hreflang="en" 
-                                                   class="link overlay-link-light">
-                                                      Home
-                                                </a>
-                                          </li>
-                                          
-                                          <li>
-                                                <a href="#" 
-                                                   hreflang="en" 
-                                                   class="link overlay-link-light">
-                                                      About us
-                                                </a>
-                                          </li>
+            <?php 
+                  include './generic/header.php';
+            ?>
 
-                                          <li>
-                                                <a href="#" 
-                                                   hreflang="en" 
-                                                   class="link overlay-link-light">
-                                                      Dictionary
-                                                </a>
-                                          </li>
-                                    </ul>
-                              </div>
-                        </nav>
-
-                        <nav class="header-cover">
-                              <div class="page-boundary">
-                                    <ul class="horizontal-menu menu-even site-menu">
-                                          <li>
-                                                <a href="#" 
-                                                   hreflang="en" 
-                                                   class="link overlay-link-light">
-                                                      Home
-                                                </a>
-                                          </li>
-
-                                          <li>
-                                                <!-- -->
-                                                <a href="#" 
-                                                   hreflang="en" 
-                                                   class="link overlay-link-light">
-                                                      What is SEO
-                                                </a> 
-                                          </li>
-
-                                          <li>
-                                                <!-- -->
-                                                <a href="#" 
-                                                   hreflang="en" 
-                                                   class="link overlay-link-light">
-                                                      SEO in practice
-                                                </a>
-                                          </li>
-                                          
-                                          <li>
-                                                <!-- -->
-                                                <a href="#" 
-                                                   hreflang="en" 
-                                                   class="link overlay-link-light">
-                                                      Themes
-                                                </a>
-                                          </li>
-
-                                          <li>
-                                                <!-- Search -->
-                                                <a href="#" 
-                                                   hreflang="en" 
-                                                   class="link overlay-link-light">
-                                                      Search
-                                                </a>
-                                          </li>
-                                    </ul>
-                              </div>
-                        </nav>
-            </header>
-            
             <main>
-                  Main Area
+                  <!-- -->
+                  <section> 
+                        <div class="page-boundary">  
+                              <div class="offset-container">      
+                                    <div class="site-cover attach-to-header">       
+                                    </div>
+                              </div>
+                        </div>
+                  </section>
+
+                  <!-- -->
+                  <section> 
+                        <div class="page-boundary site-introduction"> 
+                              <p class="document-title"> 
+                                    Areas 
+                              </p>
+                              <div class="stacked-3-containers-horizontal document-body"> 
+                                    <div class="containers"> 
+                                          Test 1
+                                    </div>
+                                    
+                                    <div class="containers">  
+                                          Test 2
+                                    </div>
+
+                                    <div class="containers"> 
+                                          Test 3
+                                    </div>
+                              </div>
+                        </div>
+                  </section>
+
+                  <!-- -->
+                  <section>
+                        <div class="page-boundary">
+                              Section 2
+                        </div>  
+                  </section>
+
+                  <!-- -->
+                  <section> 
+                        <div class="page-boundary">
+                              Section 3
+                        </div>  
+                  </section>
+
+                  <!-- -->
+                  <section> 
+                        <div class="page-boundary">
+                              Section 4
+                        </div>  
+                  </section>
+
+                  
             </main>
 
-            <footer>
-                  <div class="footer-cover">
-                        <div class="page-boundary">
-                              <!-- Container-->
-                              <nav class="horizontal-link-menu menu-area-container site-menu">
-                                          <!-- Container-->
-                                          <div class="menu-container">
-                                                <ul class="vertical-menu">
-                                                      <li>
-                                                            <p>
-                                                                  Menu
-                                                            </p>
-                                                      </li>
-                                                      <li>
-                                                            <a href="#" 
-                                                               hreflang="en" 
-                                                               class="link overlay-link">
-                                                                  Example
-                                                            </a>
-                                                      </li>
-                                                      <li>
-                                                            <a href="#" 
-                                                               hreflang="en" 
-                                                               class="link overlay-link">
-                                                                  Example
-                                                            </a>
-                                                      </li>
-                                                      <li>
-                                                            <a href="#" 
-                                                               hreflang="en" 
-                                                               class="link overlay-link">
-                                                                  Example
-                                                            </a>
-                                                      </li>
-                                                      <li>
-                                                            <a href="#" 
-                                                               hreflang="en" 
-                                                               class="link overlay-link">
-                                                                  Example
-                                                            </a>
-                                                      </li>
-                                                </ul>
-                                          </div>
-                                          <!-- Container-->
-                                          <div class="menu-container">
-                                                <ul class="vertical-menu">
-                                                      <li>
-                                                            <p>
-                                                                  Related
-                                                            </p>
-                                                      </li>
-                                                      <li>
-                                                            <a href="#" 
-                                                               hreflang="en" 
-                                                               class="link overlay-link">
-                                                                  Example
-                                                            </a>
-                                                      </li>
-                                                      <li>
-                                                            <a href="#" 
-                                                               hreflang="en" 
-                                                               class="link overlay-link">
-                                                                  Example
-                                                            </a>
-                                                      </li>
-                                                      <li>
-                                                            <a href="#" 
-                                                               hreflang="en" 
-                                                               class="link overlay-link">
-                                                                  Example
-                                                            </a>
-                                                      </li>
-                                                      <li>
-                                                            <a href="#" 
-                                                               hreflang="en" 
-                                                               class="link overlay-link">
-                                                                  Example
-                                                            </a>
-                                                      </li>
-                                                </ul>
-                                          </div>
-                                          <!-- Container-->
-                                          <div class="menu-container">
-                                                <ul class="vertical-menu">
-                                                      <li>
-                                                            <p>
-                                                                  Help
-                                                            </p>
-                                                      </li>
-                                                      <li>
-                                                            <a href="#" 
-                                                               hreflang="en" 
-                                                               class="link overlay-link">
-                                                                  Example
-                                                            </a>
-                                                      </li>
-                                                      <li>
-                                                            <a href="#" 
-                                                               hreflang="en" 
-                                                               class="link overlay-link">
-                                                                  Example
-                                                            </a>
-                                                      </li>
-                                                      <li>
-                                                            <a href="#" 
-                                                               hreflang="en" 
-                                                               class="link overlay-link">
-                                                                  Example
-                                                            </a>
-                                                      </li>
-                                                      <li>
-                                                            <a href="#" 
-                                                               hreflang="en" 
-                                                               class="link overlay-link">
-                                                                  Example
-                                                            </a>
-                                                      </li>
-                                                </ul>
-                                          </div>
-                                          <!-- Container-->
-                                          <div class="menu-container">
-                                                <div class="company-information">
-                                                      <p> 
-                                                            <a href="#" 
-                                                               hreflang="en" 
-                                                               class="link overlay-link">
-                                                                  SEO
-                                                            </a> 
-                                                      </p>
-                                                      <p>
-                                                            Description
-                                                      </p>
-                                                      <p>
-
-                                                      </p>
-                                                </div>                                              
-                                          </div>
-                              </nav>
-                        </div>
-                  </div>
-                  <!-- -->
-                  <nav>  
-                        <div class="page-boundary footer-misc-navigation">
-                              <ul class="horizontal-menu menu-even site-menu footer-misc ">
-                                    <li>
-                                          <a href="#" 
-                                             hreflang="en" 
-                                             class="link overlay-link-light">
-                                                Contact us
-                                          </a>
-                                    </li>
-
-                                    <li>
-                                          <a href="#" 
-                                             hreflang="en" 
-                                             class="link overlay-link-light">
-                                                Terms of service
-                                          </a>
-                                    </li>
-
-                                    <li>
-                                          <a href="#" 
-                                             hreflang="en" 
-                                             class="link overlay-link-light">
-                                                Cookie Policy
-                                          </a>
-                                    </li>
-
-                                    <li>
-                                          <a href="#" 
-                                             hreflang="en" 
-                                             class="link overlay-link-light">
-                                                GDPR
-                                          </a>
-                                    </li>
-                              </ul>
-                        </div>
-                  </nav>
-                  <div class="page-boundary footer-misc">
-                        <p>
-                              Copyright 2019 SEO, Kent Madsen All rights reserved
-                        </p>
-                  </div>
-            </footer>
+            <?php 
+                  include './generic/footer.php';
+            ?>
       </body>
+
+      <?php 
+            include './generic/frameworks.php';
+      ?>
 </html>
