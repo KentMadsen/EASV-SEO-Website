1418c1767ebb75a347d7465fb6b656e792709ffd
Kent Madsen <20760795+KentMadsen@users.noreply.github.com>
2019-03-06 21:14:39 +0100

    header reformated

--- site/mockup/generic/header.php
+++ site/mockup/generic/header.php
@@ -1,85 +1,85 @@
 <header>    
-<nav class="site-nav">            
-<div class="page-boundary">
-<ul class="horizontal-menu menu-left site-menu">
-<li>
-<a href="./index.php" 
-hreflang="en" 
-class="link overlay-link-light">
-Home
-</a>
-</li>
-                                          
-<li>
-<a href="./about.php" 
-hreflang="en" 
-class="link overlay-link-light">
-About us
-</a>
-</li>
+      <nav class="site-nav">            
+            <div class="page-boundary">
+            <ul class="horizontal-menu menu-left site-menu">
+                  <li>
+                        <a href="./index.php" 
+                           hreflang="en" 
+                           class="link overlay-link-light">
+                              Home
+                        </a>  
+                  </li>
+                                                      
+                  <li>
+                        <a href="./about.php" 
+                           hreflang="en" 
+                           class="link overlay-link-light">
+                              About us
+                        </a>
+                  </li>
 
-<li>
-<a href="./dictionary.php" 
-hreflang="en" 
-class="link overlay-link-light">
-Dictionary
-</a>
-</li>
+                  <li>
+                        <a href="./dictionary.php" 
+                           hreflang="en" 
+                           class="link overlay-link-light">
+                              Dictionary
+                        </a>
+                  </li>
 
-</ul>
+            </ul>
 
-</div>
-</nav>
-                        
-<!-- -->
-<nav class="header-cover">
-<div class="page-boundary">
-<ul class="horizontal-menu menu-even site-menu">
-<li>
-<!-- -->
-<a href="./index.php" 
-hreflang="en" 
-class="link overlay-link">
-Home
-</a>
-</li>
+            </div>
+      </nav>
+                              
+      <!-- -->
+      <nav class="header-cover">
+      <div class="page-boundary">
+      <ul class="horizontal-menu menu-even site-menu">
+      <li>
+      <!-- -->
+      <a href="./index.php" 
+      hreflang="en" 
+      class="link overlay-link">
+      Home
+      </a>
+      </li>
 
-<li>
-<!-- -->
-<a href="./whatisseo.php" 
-hreflang="en" 
-class="link overlay-link">
-What is SEO ?
-</a> 
-</li>
+      <li>
+      <!-- -->
+      <a href="./whatisseo.php" 
+      hreflang="en" 
+      class="link overlay-link">
+      What is SEO ?
+      </a> 
+      </li>
 
-<li>
-<!-- -->
-<a href="./seoinpractice.php" 
-hreflang="en" 
-class="link overlay-link">
-SEO in practice ?
-</a>
-</li>
-                                          
-<li>
-<!-- -->
-<a href="./theme.php" 
-hreflang="en" 
-class="link overlay-link">
-Themes
-</a>
-</li>
+      <li>
+      <!-- -->
+      <a href="./seoinpractice.php" 
+      hreflang="en" 
+      class="link overlay-link">
+      SEO in practice ?
+      </a>
+      </li>
+                                                
+      <li>
+      <!-- -->
+      <a href="./theme.php" 
+      hreflang="en" 
+      class="link overlay-link">
+      Themes
+      </a>
+      </li>
 
-<li>
-<!-- Search -->
-<a href="./search.php" 
-hreflang="en" 
-class="link overlay-link">
-Search
-</a>
-</li>
-</ul>
-</div>
-</nav>
+      <li>
+      <!-- Search -->
+      <a href="./search.php" 
+      hreflang="en" 
+      class="link overlay-link">
+      Search
+      </a>
+      </li>
+      </ul>
+      </div>
+      </nav>
 </header>
