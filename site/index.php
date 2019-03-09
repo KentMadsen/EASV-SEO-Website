b5408fb8df6a74e6ded3dc2442f1121934448ffd
Kent vejrup Madsen <20760795+KentMadsen@users.noreply.github.com>
2019-03-09 18:25:06 +0100

    a

--- site/index.php
+++ site/index.php
@@ -17,7 +17,6 @@
                   <!-- Welcome to the site -->
                   <section>
                         
-
                         <!-- -->
                         <div class="page-boundary">
                           <div>
@@ -34,108 +33,6 @@
                         </div>
 
                 </section>
-
-                <!-- Reason to use the optimisation techniques -->
-                <section class="site-introduction cover-background-main-menu">
-                        <div class="page-boundary">
-                              <p class="document-title text-center">
-                                Reasons to consider SEO optimising your site.
-                              </p>
-
-                              <p>
-                                Placeholder
-                              </p>
-
-                              <div class="stacked-3-cards-full document-body">
-                                <!-- -->
-                                    <div class="containers">
-                                          <a class="info-card-area"
-                                             href="http://www.internetlivestats.com/google-search-statistics/">
-                                                <div class="info-card infobox-server infobox-border">
-                                                      <p class="info-card-text">
-                                                            Google processes over 40.000 search quries every seconds.
-                                                      </p>
-                                                      <div class="infocard-source-button">
-                                                            <div class="infocard-sb-container">
-                                                                  <p>
-                                                                        Source
-                                                                  </p>
-                                                            </div>
-
-                                                            <div class="infocard-sb-container">
-                                                                  <div class="infocard-expandeble-container">
-                                                                        <div class="infobox-icon-continue">
-                                                                              <i class="fas fa-chevron-right">
-                                                                              </i>
-                                                                        </div>
-                                                                  </div>
-                                                            </div>
-                                                      </div>
-
-                                                </div>
-                                          </a>
-                                    </div>
-
-                                    <!-- -->
-                                    <div class="containers">
-                                          <a class="info-card-area"
-                                             href="https://moz.com/blog/state-of-searcher-behavior-revealed">
-                                              <div class="info-card infobox-browser infobox-border">
-                                                      <p class="info-card-text">
-                                                            The average google search session is below a minute.
-                                                      </p>
-                                                      <div class="infocard-source-button">
-                                                            <div class="infocard-sb-container">
-                                                                  <p>
-                                                                        Source
-                                                                  </p>
-                                                            </div>
-                                                            
-                                                            <div class="infocard-sb-container">
-                                                                  <div class="infocard-expandeble-container">
-                                                                        <div class="infobox-icon-continue">
-                                                                              <i class="fas fa-chevron-right">
-                                                                              </i>
-                                                                        </div>
-                                                                  </div>
-                                                            </div>
-                                                      </div>
-                                                </div>
-                                          </a>
-                                    </div>
-
-                                    <!-- -->
-                                    <div class="containers">
-                                          <a class="info-card-area"
-                                             href="https://www.statista.com/statistics/216573/worldwide-market-share-of-search-engines/">
-                                                <div class="info-card infobox-desktop infobox-border">
-                                                      <p class="info-card-text">
-                                                            90% of searches made on desktop uses google.
-                                                      </p>
-                                                      <div class="infocard-source-button">
-                                                            <div class="infocard-sb-container">
-                                                                  <p>
-                                                                        Source
-                                                                  </p>
-                                                            </div>
-                                                            <div class="infocard-sb-container">
-                                                                  <div class="infocard-expandeble-container">
-                                                                        <div class="infobox-icon-continue">
-                                                                              <i class="fas fa-chevron-right">
-                                                                              </i>
-                                                                        </div>
-                                                                  </div>
-                                                            </div>
-                                                      </div>
-                                                </div>
-                                          </a>
-                                    </div>
-                              </div>
-
-                              <div>
-
-                        </div>
-                  </section>
 
                   <!-- Short introduction -->
                   <section class="cover-background site-introduction">
@@ -155,16 +52,6 @@
                         </div>
                   </section>
 
-                  <!-- -->
-                  <section>
-                        <div class="page-boundary">
-                              <div class="offset-container">
-                                    <div class="site-cover attachable site-cover-end offset-to-footer">
-
-                                    </div>
-                              </div>
-                        </div>
-                  </section>
             </main>
 
             <?php include './generic/footer.html'; ?>
