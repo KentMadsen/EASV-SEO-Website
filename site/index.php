e9da4a934c895ad2c2706ee63f2be426a5dae9e0
Kent vejrup Madsen <20760795+KentMadsen@users.noreply.github.com>
2019-03-10 22:03:09 +0100

    a

--- site/index.php
+++ site/index.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
 <html>
-      <head>
-            <meta name="description" content="" />
-            <meta keywords="" content="" />
-            <meta author="" content="" />
+      <head>      
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <meta name="description" content=" "/>
+            <meta name="keywords" content="Search Engine Optimization, SEO, Backend, Frontend"/>
+            <meta name="robots" content="index, follow"/>
+            <meta name="author" content="Kent Madsen"/>
+            <meta name="msvalidate.01" content="A61A16D254E6847F3E8AEAF2A3B37922" />
+
+            <!-- https://www.wordstream.com/blog/ws/2015/04/30/seo-basics -->
             <title>
-                  Search Engine Optimisation - Application
+                  Search Engine Optimisation - 
             </title>
 
             <?php include "./generic/libraries.html"; ?>
@@ -93,10 +100,26 @@
                                 Search Engine Optimisation
                             </h1>
 
-                            <!-- -->
+                            <h2> 
+                                  What is SEO. 
+                            </h2>
+                            <!-- https://www.interaction-design.org/literature/topics/search-engine-optimization -->
                             <p class="website-description">
                                 Search engine optimisation (SEO.) is a set of techniques used with the intention to improve a sites visibility on the internet.
-                                By making data easily accessible for search engines to parse. There by gaining attention or sales by increasing the sites rank, It's a tedious process, which require technical and business decisions.
+                                By making data easily accessible for search engines to parse and increase the sites rank. 
+                                Which in turn will gain attention or sales for the website.
+
+                                It's a tedious process, which require technical and business decisions on the site and might not garantee results. 
+                                But is usefull for futurer use;
+                            </p>
+
+                            <h2> 
+                                  How it's used  
+                            </h2>
+                            <!-- -->
+                            <p class="website-description">
+                              Search engine optimisation can be done at several stages from easy to implement changes, 
+                              to rewriting frontend and backend components and restructering the organisation of the information.
                             </p>
                           </div>
                         </div>
