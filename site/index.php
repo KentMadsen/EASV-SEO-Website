fc772b1d785b750972a438f4b31136d412669d5b
Kent vejrup Madsen <20760795+KentMadsen@users.noreply.github.com>
2019-03-10 18:40:38 +0100

    removed repository

--- site/index.php
+++ site/index.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html>
       <head>
-            <meta />
+            <meta name="description" content="" />
+            <meta keywords="" content="" />
+            <meta author="" content="" />
             <title>
                   Search Engine Optimisation - Application
             </title>
@@ -14,72 +16,75 @@
 
             <!-- Content Area  -->
             <main>
-                  <section> 
-                        <div id="interactive-frontpage" 
-                             class="offset-container"> 
+                  <section>
+                    <!--
+                        <div id="interactive-frontpage"
+                             class="offset-container">
+
                               <div class="header-cover offset-to-header">
-                                    <canvas id="interactive-frontpage-canvas" 
-                                            width="1900px" 
-                                            height="550px"> 
+                                    <canvas id="interactive-frontpage-canvas"
+                                            width="1900px"
+                                            height="550px">
                                     </canvas>
                               </div>
-                        </div>
-                        <div class="cover-background-main-menu"> 
-                              <ol class="frontpage-share-list"> 
-                                    <li> 
-                                          <a href="https://www.facebook.com/" 
+                        </div>-->
+                        <div class="cover-background-main-menu">
+                              <ol class="frontpage-share-list">
+                                    <li>
+                                          <a href="https://www.facebook.com/"
                                              class="site-link frontpage-share-list-button">
                                                 <div class="frontpage-share-list-button-icon">
-                                                      <i class="fab fa-facebook">      
+                                                      <i class="fab fa-facebook">
                                                       </i>
-                                                </div> 
-                                                <div class="frontpage-share-list-button-text"> 
-                                                      <p> 
+                                                </div>
+                                                <div class="frontpage-share-list-button-text">
+                                                      <p>
                                                             Facebook
                                                       </p>
                                                 </div>
                                           </a>
                                     </li>
 
-                                    <li> 
-                                          <a href="https://www.linkedin.com/company/designermadsen" 
-                                             class="site-link frontpage-share-list-button"> 
-                                                <div class="frontpage-share-list-button-icon">
-                                                      <i class="fab fa-linkedin-in">
-                                                      </i>
-                                                </div>
-                                                <div class="frontpage-share-list-button-text"> 
-                                                      <p> 
-                                                            Linkedin
-                                                      </p>
-                                                </div>
-                                          </a>
-                                    </li>
-                                                            
-                                    <li> 
-                                          <a href="https://twitter.com/" 
+                                    <li>
+                                          <a href="https://twitter.com/"
                                              class="site-link frontpage-share-list-button">
                                                 <div class="frontpage-share-list-button-icon">
                                                       <i class="fab fa-twitter-square">
-                                                      </i> 
+                                                      </i>
                                                 </div>
-                                                <div class="frontpage-share-list-button-text"> 
-                                                      <p> 
+                                                <div class="frontpage-share-list-button-text">
+                                                      <p>
                                                             Twitter
                                                       </p>
                                                 </div>
                                           </a>
                                     </li>
 
-                                    <li> 
-                                          <a href="https://github.com/KentMadsen/SEO-Website" 
+                                    
+                                    <li>
+                                          <a href="https://www.linkedin.com/company/designermadsen"
+                                             class="site-link frontpage-share-list-button">
+                                                <div class="frontpage-share-list-button-icon">
+                                                      <i class="fab fa-linkedin-in">
+                                                      </i>
+                                                </div>
+                                                <div class="frontpage-share-list-button-text">
+                                                      <p>
+                                                            Linkedin
+                                                      </p>
+                                                </div>
+                                          </a>
+                                    </li>
+
+                                    <li>
+                                          <a href="https://github.com/KentMadsen/SEO-Website"
                                              class="site-link frontpage-share-list-button">
                                                 <div class="frontpage-share-list-button-icon">
                                                       <i class="fab fa-github-square">
-                                                      </i> 
+                                                      </i>
                                                 </div>
-                                                <div class="frontpage-share-list-button-text"> 
-                                                      <p> 
+                                                <div class="frontpage-share-list-button-text">
+                                                      <p>
                                                             Github
                                                       </p>
                                                 </div>
@@ -90,7 +95,7 @@
                   </section>
                   <!-- Welcome to the site -->
                   <section>
-                        
+
                         <!-- -->
                         <div class="page-boundary">
                           <div>
@@ -101,7 +106,8 @@
 
                             <!-- -->
                             <p class="website-description">
-                                Search engine optimisation (SEO.) is a set of techniques used with the intention to improve a sites visibility on the internet. By making data accessible for search engines to parse and make available. There by gaining attention or sales, It's a tedious process, which require technical and business decisions.
+                                Search engine optimisation (SEO.) is a set of techniques used with the intention to improve a sites visibility on the internet.
+                                By making data easily accessible for search engines to parse. There by gaining attention or sales by increasing the sites rank, It's a tedious process, which require technical and business decisions.
                             </p>
                           </div>
                         </div>
@@ -130,14 +136,14 @@
 
             <?php include './generic/footer.html'; ?>
       </body>
-      
+
       <?php include './generic/frameworks.html';
             include './generic/code.html';
             include './generic/repository.html';
       ?>
 
       <!-- Content Area  -->
-      <script type=""> 
+      <script type="application/javascript">
             initialise_front();
       </script>
 </html>
